b486c61ea1515a5bd597431a963f68cf6e534fc2
Korgan Rivera <korganrivera@gmail.com>
2019-04-04 20:32:21 -0500

    Got backprop working for one weight so far.

--- snn.c
+++ snn.c
@@ -48,15 +48,24 @@ typedef struct{
     double sigmoid;
     double dsig_dsum;
     double dsum_dw;
+    double dsig_dw;
     double *w_adjustment;
 }neuron;
+
+typedef struct{
+    double diff;
+    double sq_err;
+    double de_dsig;
+}error;
+
+
 
 // points to each layer and each neuron. Layers may have different depths.
 neuron **network;
 
 int main(int argc, char **argv){
     unsigned i, j, k;
-    unsigned node_count[] = {2, 3, 2, 4, 5, 3, 5, 2, 1}; // Gives me random node counts for each layer for testing with.
+    unsigned node_count[] = {2, 2, 2, 4, 5, 3, 5, 2, 1}; // Gives me random node counts for each layer for testing with.
 
     // test data.
     int data[][4] = {
@@ -82,9 +91,6 @@ int main(int argc, char **argv){
         }
     }
 
-    // malloc space for squared error vector.
-    double *error_vector= malloc(node_count[LAYERS - 1] * sizeof(double));
-
     // malloc space for y_pred.
     double *error_sq_vector = malloc(node_count[LAYERS - 1] * sizeof(double));
 
@@ -100,14 +106,18 @@ int main(int argc, char **argv){
         }
     }
 
-    // initialise vectors.
+    // malloc space for error vector.
+    error *error_vector = malloc(node_count[LAYERS - 1] * sizeof(error));
+
+    // initialise error vectors.
     for(j = 0; j < node_count[LAYERS - 1]; j++){
-        error_vector[j] = 0.0;
-        error_sq_vector[j] = 0.0;
+        error_vector[j].diff    = 0.0;
+        error_vector[j].sq_err  = 0.0;
+        error_vector[j].de_dsig = 0.0;
     }
 
     // process each data point.
-    for(unsigned data_index = 0; data_index < 4; data_index++){
+    for(unsigned data_index = 0; data_index < 1; data_index++){
 
         puts("\nnew data:");
         // load one data point into first layer.
@@ -115,90 +125,62 @@ int main(int argc, char **argv){
             network[0][i].sigmoid = data[data_index][i];
         }
 
-        // feed forward.
+        // feed forward and do some backprop partial derivatives.
         for(i = 1; i < LAYERS; i++){
             for(j = 0; j < node_count[i]; j++){
                 for(k = 0; k < node_count[i - 1]; k++){
                     network[i][j].sum += network[i][j].weight[k] * network[i - 1][k].sigmoid;
                 }
-                printf("network[%u][%u].sum = %lf\n", i, j, network[i][j].sum);
+                network[i][j].sum += network[i][j].bias;
+
                 network[i][j].sigmoid = sigmoid(network[i][j].sum);
+                network[i][j].dsig_dsum = deriv_sigmoid(network[i][j].sum);
+                if(i == 1){
+                    if(j == 0)
+                        network[i][j].dsum_dw = network[0][0].sigmoid;
+                    else
+                        network[i][j].dsum_dw = 0;
+                }
+                else{
+                    network[i][j].dsum_dw = network[i][j].weight[0] * network[i - 1][0].dsig_dw + network[i][j].weight[1] * network[i - 1][1].dsig_dw;
+                }
+
+                network[i][j].dsig_dw = network[i][j].dsig_dsum * network[i][j].dsum_dw;
+
+                printf("network[%u][%u].sum = %lf\n", i, j, network[i][j].sum);
                 printf("network[%u][%u].sigmoid = %lf\n", i, j, network[i][j].sigmoid);
+                printf("network[%u][%u].dsig_dsum = %lf\n", i, j, network[i][j].dsig_dsum);
+                printf("network[%u][%u].dsum_dw = %lf\n", i, j, network[i][j].dsum_dw);
+                printf("network[%u][%u].dsig_dw = %lf\n", i, j, network[i][j].dsig_dw);
+
             }
         }
 
-        // backprop: calc all partial derivatives for each output.
-        // This is spaghetti.
-
-        // calc error_vector, error_sq_vector, and total_error.
+        // calc diff, squared error, total error.
         double total_error = 0.0;
         for(j = 0; j < node_count[LAYERS - 1]; j++){
-            error_vector[j] = data[data_index][node_count[0] + j] - network[LAYERS - 1][j].sigmoid;
-            error_sq_vector[j] = error_vector[j] * error_vector[j];
-            printf("sq_error: %lf\n", error_sq_vector[j]);
-            total_error += error_sq_vector[j];
+            error_vector[j].diff = data[data_index][node_count[0] + j] - network[LAYERS - 1][j].sigmoid;
+            error_vector[j].sq_err = error_vector[j].diff * error_vector[j].diff;
+            printf("sq_error: %lf\n", error_vector[j].sq_err);
+            error_vector[j].de_dsig = -2.0 * error_vector[j].diff;
+            total_error += error_vector[j].sq_err;
         }
-        total_error /= node_count[LAYERS - 1];
         printf("total_error: %lf\n", total_error);
 
-        // calculate d(total_error) / d(w)
-        // dt/dw = ...
 
-
-
-        double *dL_dypredw = malloc(node_count[LAYERS - 2] * sizeof(double));
-        double **dh_dw = malloc(node_count[LAYERS - 2] * sizeof(double));
-        for(i = 0; i < node_count[LAYERS - 2]; i++){
-            dh_dw[i] = malloc(node_count[LAYERS - 3] * sizeof(double));
+        // calculate dL/dw.
+        double dLdw = 0.0;
+        for(j = 0; j < node_count[LAYERS -1]; j++){
+            dLdw += error_vector[j].de_dsig * network[LAYERS -1][j].dsig_dw;
+            printf("de_dsig:%lf, dsig_dw: %lf\n", error_vector[j].de_dsig, network[LAYERS -1][j].dsig_dw);
         }
 
+        printf("dLdw for w0: %lf\n", dLdw);
 
-        /* So, here's the deal with the partial derivatives.
-         * (after about 6 hours of studying the problem...)
-         * Given a weight or bias, I think I have an algorithm that will give
-         * me the correct partial derivatives to calculate in the right order.
-         * If the given weight is n[i][j].weight[k], then I need to calc
-         *
-         *    d n[p][q].sum
-         * -------------------   where p = i, i+1, i+2, ... I
-         * d n[i][j].weight[k]         q = 0, 1, 2,     ... J
-         *
-         *  and
-         *
-         *  d n[p][q].sigmoid
-         * -------------------   where p = i, i+1, i+2, ... I
-         * d n[i][j].weight[k]         q = 0, 1, 2,     ... J
-         *
-         * for all weights. And obviously also
-         *
-         *   d n[p][q].sum       where p = i, i+1, i+2, ... I
-         * ------------------          q = 0, 1, 2,     ... J
-         *   d n[i][j].bias
-         *
-         *  d n[p][q].sigmoid
-         * -------------------   where p = i, i+1, i+2, ... I
-         *   d n[i][j].bias            q = 0, 1, 2,     ... J
-         *
-         *  for all biases.
-         *
-         * When I include the sigmoid_deriv() function, then I have all the
-         * parts I need to calculate all the partial derivatives for all the
-         * weights and biases. Debugging this will be a fucking nightmare, so
-         * get it right the first time!
-         *
-         *
-         * Fri Mar 29 21:30:11 CDT 2019
-         * Pretty excited! After about a million hours, I think I've come up
-         * with the simplest way to calculate all those partial derivatives.
-         * I'm going to add two fields to my neurons, and they are dsig_dsum
-         * and dsum_dw. Starting at the layer that the weight in question is, I
-         * calculate those values for each neuron in that layer. Then I repeat
-         * for the layers ahead of it until I reach the last neurons. That
-         * gives me dypred_dw. I use that to calculate dL_dypred, which is
-         * easy.  And that's all there is too it! Isn't that insane!? The
-         * entire nest of partial derivatives collapses to a super simple
-         * algorithm!
-         * */
+        // adjust weight accordingly.
+        network[1][0].weight[0] -= LR * dLdw;
+
+        printf("subtracting %lf from w[0]\n", LR * dLdw);
 
     } // each data point processed.
 
